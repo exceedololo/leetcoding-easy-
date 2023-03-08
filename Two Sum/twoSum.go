b27c3d1ada65dd43e3b9aaae9c1432840fe333a1
exceedololo <exceedololo@outlook.com>
2023-03-08 18:37:27 +0300

    true

--- Two Sum/twoSum.go
+++ Two Sum/twoSum.go
@@ -1,1 +1,13 @@
-package Two_Sum
+func twoSum(nums []int, target int) []int {
+	mappa := make(map[int]int)
+	for i, num := range nums{
+		mappa[num] = i
+	}
+	for i, num := range nums{
+		diff := target - num
+		if j, ok := mappa[diff]; ok && j != i{
+			return []int{j, i}
+		}
+	}
+	return []int{}
+}
