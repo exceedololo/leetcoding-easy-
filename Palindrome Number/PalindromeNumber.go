b933be95078b50be306f1d47cd4f084b668193d3
Alexander Zhdanov <77617952+exceedololo@users.noreply.github.com>
2023-03-10 09:57:14 +0300

    Update PalindromeNumber.go

--- Palindrome Number/PalindromeNumber.go
+++ Palindrome Number/PalindromeNumber.go
@@ -1,12 +1,12 @@
 func isPalindrome(x int) bool {
-str := strconv.Itoa(x)
-pravda:= true
-dlina := len(str)
-for i := 0; i < dlina/2; i++ {
-if str[i] != str[dlina-i-1] {
-pravda = false
-break
-}
-}
-return pravda
+  str := strconv.Itoa(x)
+  pravda:= true
+  dlina := len(str)
+  for i := 0; i < dlina/2; i++ {
+    if str[i] != str[dlina-i-1] {
+    pravda = false
+    break
+    }
+  }
+  return pravda
 }
