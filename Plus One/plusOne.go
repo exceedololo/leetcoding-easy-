8548e9858792e9daa231b410accd08faec6f1d2b
exceedololo <exceedololo@outlook.com>
2023-03-17 10:35:40 +0300

    +

---
+++ Plus One/plusOne.go
@@ -0,0 +1,17 @@
+package plusOne
+
+func plusOne(digits []int) []int {
+	carry := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] += carry
+		if digits[i] <= 9 {
+			carry = 0
+			break
+		}
+		digits[i] = 0
+	}
+	if carry > 0 {
+		digits = append([]int{1}, digits...)
+	}
+	return digits
+}
