616ae433179bd59ef2c6278e4c5d84f49dd841b0
Alexander Zhdanov <77617952+exceedololo@users.noreply.github.com>
2023-04-01 19:20:58 +0300

    Update mergSorted.go

--- Merge Sorted Array/mergSorted.go
+++ Merge Sorted Array/mergSorted.go
@@ -1,4 +1,22 @@
+//first one is probably bad cause isn't optimal
 func merge(nums1 []int, m int, nums2 []int, n int)  {
 	copy(nums1[m:], nums2)
 	sort.Ints(nums1)
 }
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    i, j:= m-1, n-1
+    for i >= 0 && j >= 0{
+        if nums1[i] > nums2[j]{
+            nums1[i+j+1] = nums1[i]
+            i--
+        }else{
+            nums1[i+j+1] = nums2[j]
+            j--
+        }
+    }
+    for j >= 0 {
+        nums1[j] = nums2[j]
+        j--
+    }
+} 
